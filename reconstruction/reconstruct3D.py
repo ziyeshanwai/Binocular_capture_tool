71f4e4390478f481db40b1491b00c12f50401a61
LiyouWang <1085066875@qq.com>
2019-09-17 16:56:54 +0800

    增加重建可视化显示，图片以及3D点显示

--- reconstruction/reconstruct3D.py
+++ reconstruction/reconstruct3D.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # 空间三维画图
+import os
 
 
 def get_AB(Ml, Mr, ul, vl, ur, vr):
@@ -27,15 +28,15 @@ def get_AB(Ml, Mr, ul, vl, ur, vr):
 
 
 if __name__ == "__main__":
-
-    ul = 40  # 左侧图片像素坐标
-    vl = 60
-    ur = 30  # 右侧图片像素坐标
-    vr = 20
+    left_img_root_path = r"\\192.168.20.63\ai\Liyou_wang_data\double_cameras_video\imgs\left"
+    right_img_root_path = r"\\192.168.20.63\ai\Liyou_wang_data\double_cameras_video\imgs\right"
+    left_img = cv2.imread(os.path.join(left_img_root_path, "0.jpg"))
+    right_img = cv2.imread(os.path.join(right_img_root_path, "0.jpg"))
+    radius = 10
     Ml = np.array([[10, 0, 360, 0], [0, 10, 640, 0], [0, 0, 1, 0]], dtype=np.float32)
-    Mr = np.array([[5.241245912283468, -4.804079641277808, -15.62010463754545, 20.71041494220587],
-                   [5.012054817985312, -7.51852747759903, -47.34634706530864, 42.5533817579683],
-                   [0.008611215279209808, -0.01194003359547416, -0.07051263005552549, 0.05128554702553706]], dtype=np.float32)  # 右侧相机矩阵
+    Mr = np.array([[0.3520066989851156, 2.073537362795778, -18.45796152964473, -28.37748590946849],
+                   [2.508549580148226, 2.247961998832718, -13.69927652036192, -43.29439392369385],
+                   [0.00274310953611928, 0.00441668809134959, -0.03340103384464285, -0.0576053978033833]], dtype=np.float32)  # 右侧相机矩阵
     left_uvs_list = [[185, 186], [271, 199], [366, 201], [467, 191], [536, 201], [152, 265], [508, 280], [316, 321], [279, 540], [255, 665], [287,678],[320,669],[239,723],[291,747],[350,742],[289,846]]
     right_uvs_list = [[298, 213], [397, 235], [485, 243], [557, 241], [594, 251], [249, 296], [586, 324], [442, 361], [491, 583], [420, 716], [457,722],[481,707],[386,785],[445,791],[489,774],[430,894]]
     x_ = []
@@ -58,10 +59,20 @@ if __name__ == "__main__":
         xw = np.append(x_[i], 1)
         uvs = Ml.dot(xw[:, np.newaxis])
         uvs = uvs/uvs[2]
+        cv2.circle(left_img, (int(uvs[0]), int(uvs[1])), radius, (255, 0, 0), 1)
+        cv2.putText(left_img, "{}".format(i), (int(uvs[0] - radius / 2), int(uvs[1] + radius / 2)), cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (255, 255, 255), lineType=cv2.LINE_AA)
         print("left:{} {}".format(i, uvs))
         uvs = Mr.dot(xw[:, np.newaxis])
         uvs = uvs / uvs[2]
+        cv2.circle(right_img, (int(uvs[0]), int(uvs[1])), radius, (255, 0, 0), 1)
+        cv2.putText(right_img, "{}".format(i), (int(uvs[0] - radius / 2), int(uvs[1] + radius / 2)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (255, 255, 255), lineType=cv2.LINE_AA)
         print("right:{} {}".format(i, uvs))
+    cv2.imshow('left_image', left_img)
+    cv2.imshow('right_image', right_img)
+    # cv2.waitKey(0)
     ax.scatter(x_[:, 0], x_[:, 1], x_[:, 2])
     plt.show()
 
